045d0871947df9308c044299f036dd7a6f367bb1
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-10 14:45:51 +0700

    membuat member dari class mahasiswa

--- class_oop_157/class_oop_157.cpp
+++ class_oop_157/class_oop_157.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa() {
-
+class Mahasiswa {
 public:
-	
+	int nim;
+	string nama;
+	void tampil() {
+		cout << "NIM = " << nim;
+		cout << "\n Nama = " << nama;
+	}
 }
